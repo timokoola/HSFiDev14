f57a791121428c8f3d00abc7085682a4d0a9ce6a
Timo Koola <timo@moarub.com>
2012-03-06 22:49:17 +0200

    Settings updated

--- aino/settings.py
+++ aino/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
+	'aino.finland',
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
