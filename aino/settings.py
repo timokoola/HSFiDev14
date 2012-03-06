a9f2d9ff1fb441473389d78bb604114cf1dcf619
Timo Koola <timo@moarub.com>
2012-03-06 21:03:14 +0000

    settings from server

--- aino/settings.py
+++ aino/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/timokoola/mycode/HSFiDev14/aino/aino.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/usr/local/www/wsgi-scripts/HSFiDev14/aino/aino.db ',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aino.finland',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
